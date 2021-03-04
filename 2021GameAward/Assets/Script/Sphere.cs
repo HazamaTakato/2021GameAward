1c1fd7904c9111f146599e8cf92731fc4f1bfe31
takatonzzz <hzmtakaton@gmail.com>
2021-03-05 08:51:36 +0900

    3D球（プレイヤー）の作成・移動・拡大(Z)縮小(X)・シーンの再読み込み(R)

---
+++ 2021GameAward/Assets/Script/Sphere.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sphere : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * speed;
+        rb.AddForce(x, 0, 0);
+
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            this.transform.localScale = new Vector3(2, 2, 2);
+        }
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            this.transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}

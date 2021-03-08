b40aeeae978d166be42267d2a00fc0f2ee2a8897
takatonzzz <hzmtakaton@gmail.com>
2021-03-08 17:57:28 +0900

    大きい状態の時にだけアイテムを拾うとアイテムが消える処理

--- 2021GameAward/Assets/Script/Sphere.cs
+++ 2021GameAward/Assets/Script/Sphere.cs
@@ -7,6 +7,9 @@ public class Sphere : MonoBehaviour
 {
     public float speed = 1.0f;
     public Rigidbody rb;
+    bool normalSize = true;
+    public GameObject Item;
+    bool GetItem = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,18 +22,29 @@ public class Sphere : MonoBehaviour
         float x = Input.GetAxis("Horizontal") * speed;
         rb.AddForce(x, 0, 0);
 
-        if(Input.GetKeyDown(KeyCode.Z))
+        if(Input.GetKeyDown(KeyCode.Z)&&normalSize)
         {
             this.transform.localScale = new Vector3(2, 2, 2);
+            normalSize = false;
         }
-        if(Input.GetKeyDown(KeyCode.X))
+        if(Input.GetKeyDown(KeyCode.X)&&!normalSize)
         {
             this.transform.localScale = new Vector3(1, 1, 1);
+            normalSize = true;
         }
 
         if(Input.GetKeyDown(KeyCode.R))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Item"&&!normalSize)
+        {
+            GetItem = true;
+            Destroy(Item);
         }
     }
 }
